e8a52eaac5d0001ef892e8bb9afe8cf4754f6220
Lan Tzu Hua <ray.lan@tpisoftware>
2024-01-25 15:58:28 +0800

    docs: 補充關於 module 說明的註解

--- src/business-logic/business-logic.module.ts
+++ src/business-logic/business-logic.module.ts
@@ -3,6 +3,9 @@ import { HahowAppService } from './commons/hahow-app/hahow-app.service';
 import { HeroesService } from './heroes/heroes.service';
 import { UtilsModule } from '@root/utils/utils.module';
 
+/**
+ * 負責執行商業邏輯，以提供給 controller 叫用
+ */
 @Module({
   imports: [UtilsModule],
   providers: [HahowAppService, HeroesService],
--- src/controllers/controllers.module.ts
+++ src/controllers/controllers.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { HeroesController } from './heroes/heroes.controller';
 import { BusinessLogicModule } from '@root/business-logic/business-logic.module';
 
+/**
+ * 定義所有的 API \
+ * 負責接收 request 和回傳 response \
+ * 負責外部輸入的 validation
+ */
 @Module({
   imports: [BusinessLogicModule],
   controllers: [HeroesController],
